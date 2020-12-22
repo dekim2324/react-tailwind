20dd4760e46f2f85b6985c493d8e4c25b7169ce2
Dennis Kim <dekim2324@gmail.com>
2020-12-21 22:12:00 -0600

    dummy photo done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ const App = () => {
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by John Doe
         </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:: </strong>
+            300
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            4000
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
       </div>
     </div>
   );
